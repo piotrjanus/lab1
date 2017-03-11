a0ef1063df40c83f69de88d0f2817328ee79668a
piotrjanus <piotrjanusjanus@gmail.com>
2017-03-11 19:33:11 +0100

    added wc

---
+++ pywc.py
@@ -0,0 +1,47 @@
+import sys
+from pygrep import fileListMaker
+import logging
+import os
+
+class pywc:
+	def __init__(self, listOfFilesNotValidated):
+		self.fileListHelper = fileListMaker("")
+		self.fileListHelper.listOfFiles = listOfFilesNotValidated
+		self.fileListHelper.validateFiles()
+		self.filesName = self.fileListHelper.listOfFiles
+	
+		self.words = {}
+		self.lines = {}
+		self.chars = {}
+		os.remove('pywc.log')
+		logging.basicConfig(filename='pywc.log',level=logging.INFO)
+
+
+	def countInFiles(self):
+		for file_name in self.filesName:
+			self.countInFile(file_name)
+	
+	def countInFile(self, file_name):
+		self.lines[file_name] = 0
+		self.words[file_name] = 0
+		self.chars[file_name] = 0
+		with open(file_name, 'r') as in_file:
+			for line in in_file:
+				self.lines[file_name] += 1
+				self.words[file_name] += len(line.split())
+				self.chars[file_name] += len(line)
+
+		logging.info("%d %d %d %s",self.chars[file_name], self.words[file_name], self.lines[file_name], file_name)
+		
+
+
+def main():
+	temp_list = list(sys.argv)
+	del temp_list[0]
+	my_pywc = pywc(temp_list)
+	my_pywc.countInFiles()
+
+
+if __name__ == '__main__':
+	main()
+
